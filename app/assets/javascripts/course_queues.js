a54a796db31aa3584f1c4f7a458c8448e141ea6d
Matthew Terwilliger <matt@terwilligers.com>
2016-09-05 15:34:29 -0400

    More updates

--- app/assets/javascripts/course_queues.js
+++ app/assets/javascripts/course_queues.js
@@ -1,3 +1,31 @@
+$(document).ready(function () {
+  const elt = renderHelpForm();
+
+  elt.appendTo('[data-cable-container="action-content"]');
+
+  $('[data-cable-container="action-content"]').closest('.ui.segment').removeClass('disabled loading');
+});
+
+function renderHelpForm() {
+    const template = $('[data-cable-template="help_request_form"]');
+    const elt      = $(template.html());
+
+    return elt;
+}
+
+function renderMyRequest(request) {
+    const template = $('[data-cable-template="my-request"]');
+    const elt      = $(template.html());
+
+    elt.find('input[name="location"]').val(request.location);
+    elt.find('textarea[name="description"]').val(request.description);
+
+    $('[data-cable-container="action-content"]').html(elt);
+
+    $('[data-cable-container="action-content"]').closest('.ui.segment').removeClass('disabled loading');
+}
+
+
 function renderRequest(request) {
     const template = $('[data-cable-template=request]');
     const elt      = $(template.html());
@@ -26,6 +54,12 @@ function getOutstandingRequests() {
       const elt = renderRequest(request);
 
       elt.appendTo('[data-cable-container=requests]');
+
+
+      let userId = $('#course-queue-name').data('user-id');
+      if (request.requester_id === userId) {
+        renderMyRequest(request);
+      }
     });
 
     updateCount(data.length);
