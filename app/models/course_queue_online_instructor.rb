bbc3783266fc258d9b8ed6d895574c7157426637
Matthew Terwilliger <matt@terwilligers.com>
2016-09-06 03:32:04 -0400

    Kill unused CourseQueueOnlineInstructor methods

--- app/models/course_queue_online_instructor.rb
+++ app/models/course_queue_online_instructor.rb
@@ -1,12 +1,4 @@
 class CourseQueueOnlineInstructor < ApplicationRecord
   belongs_to :course_queue
   belongs_to :online_instructor, class_name: 'User'
-
-  def sign_in!
-    self.update!(online: true)
-  end
-
-  def sign_out!
-    self.update!(online: false)
-  end
 end
