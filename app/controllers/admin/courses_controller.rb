36c3e1079527f7406c8e438b80a2af2574ec7372
Matthew Terwilliger <matt@terwilligers.com>
2016-10-05 17:38:53 -0400

    Always call user.instructor_for_course?()
    This will ensure global admins receive the same permissions

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -12,7 +12,7 @@ class Admin::CoursesController < Admin::AdminController
   end
 
   def authorize_current_user
-    unless @course.instructors.include?(current_user)
+    unless current_user.instructor_for_course?(@course)
       redirect_to root_url
     end
   end
