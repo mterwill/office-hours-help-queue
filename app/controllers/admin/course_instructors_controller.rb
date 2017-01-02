d71d7046e2fb38736e6d07aea72ad7f72038be85
Matthew Terwilliger <matt@terwilligers.com>
2017-01-02 11:14:44 -0600

    Flesh out instructor self-enrollment
    Closes #37

--- app/controllers/admin/course_instructors_controller.rb
+++ app/controllers/admin/course_instructors_controller.rb
@@ -1,9 +1,27 @@
 class Admin::CourseInstructorsController < Admin::AdminController
-  before_action :set_and_authorize_course, only: :new
-
+  before_action :load_course, only: [:new, :promote]
   def new
-    @course_instructor = CourseInstructor.new
-    @course_instructor.course = @course
+    unless current_user.instructor_for_course?(@course)
+      redirect_to root_url
+    end
+  end
+
+  def promote
+    # get/post map to the same action
+    return render 'promote' unless request.post?
+
+    unless params[:enrollment_code] == @course_instructor.course.enrollment_code
+      # bad code
+      return redirect_to root_url
+    end
+
+    unless current_user.instructor_for_course?(@course_instructor.course)
+      # don't create duplicate instructors
+      @course_instructor.instructor = current_user
+      @course_instructor.save!
+    end
+
+    redirect_to course_url(@course_instructor.course)
   end
 
   def create
@@ -38,11 +56,9 @@ class Admin::CourseInstructorsController < Admin::AdminController
   end
 
   private
-  def set_and_authorize_course
+  def load_course
     @course = Course.find_by!(slug: params[:id])
-
-    unless current_user.instructor_for_course?(@course)
-      redirect_to root_url
-    end
+    @course_instructor = CourseInstructor.new
+    @course_instructor.course = @course
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,10 @@ Rails.application.routes.draw do
       member do
         get :statistics
         resources :course_queues, only: [:new]
-        resources :course_instructors, only: [:new]
+        resources :course_instructors, only: [:new] do
+          get :promote, on: :collection
+          post :promote, on: :collection
+        end
       end
     end
   end
