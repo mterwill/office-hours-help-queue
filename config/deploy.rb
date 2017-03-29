0f2218084641f57340c7272e56097ba1e12e81ad
Matthew Terwilliger <matt@terwilligers.com>
2017-03-29 18:45:43 -0400

    Restart task is unnecessary
    Capistrano whines about this, get rid of it, it's already being done
    elsewhere.

--- config/deploy.rb
+++ config/deploy.rb
@@ -77,7 +77,6 @@ namespace :deploy do
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
-  after  :finishing,    :restart
 end
 
 # ps aux | grep puma    # Get puma pid
