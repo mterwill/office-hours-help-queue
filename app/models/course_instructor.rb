e4f061d44e9836b44573615fcfca4f0ff363abda
Matthew Terwilliger <matt@terwilligers.com>
2018-01-09 09:07:25 -0500

    Validate the course instructor doesn't already exist

--- app/models/course_instructor.rb
+++ app/models/course_instructor.rb
@@ -1,4 +1,12 @@
 class CourseInstructor < ApplicationRecord
   belongs_to :course
   belongs_to :instructor, class_name: 'User'
+
+  validate :course_instructor_already_exists
+
+  def course_instructor_already_exists
+    if course.course_instructors.find_by(instructor: instructor)
+      errors.add(:instructor, "already exists for this course")
+    end
+  end
 end
