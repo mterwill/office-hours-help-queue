610ffd0fd7d303ca6c5050c19be7b7ffb683efd7
Matthew Terwilliger <matt@terwilligers.com>
2020-09-18 17:56:35 -0700

    env was removed in Rails 5.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def create
-    user = User.from_omniauth(env['omniauth.auth'])
+    user = User.from_omniauth(request.env['omniauth.auth'])
     cookies.signed[:user_id] = user.id
     redirect_to request.env['omniauth.origin'] || root_path
   end
