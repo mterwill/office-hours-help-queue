0966d19e39b8de39f4046c8573d86d7e1a754415
Annie <annieli@umich.edu>
2020-11-27 13:46:42 -0500

    Fix spacing for instructor buttons

--- app/assets/javascripts/components/action.js.jsx
+++ app/assets/javascripts/components/action.js.jsx
@@ -1,9 +1,7 @@
 var Action = React.createClass({
   render: function () {
     return (
-      <span style={{ paddingRight: '4px' }}>
-        <a className={this.props.data.className} onClick={this.props.data.action}>{this.props.data.title}</a>
-      </span>
+      <a className={this.props.data.className} onClick={this.props.data.action}>{this.props.data.title}</a>
     );
   }
 });
